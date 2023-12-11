69cf306888b9a7a32ff94839993e1124587ba37a
asghar <babaraliengineer32@gmail.com>
2023-12-11 21:26:07 +0500

    indigo car

--- src/testerClass.java
+++ src/testerClass.java
@@ -1,6 +1,11 @@
 public class testerClass {
     public static void main(String[] args) {
         carmanufacture car=new carmanufacture();
+        car.color="indigo";
+        car.door="one";
+        car.seats="4";
+        car.speed="100 km/h";
+        System.out.println(car.color);
 
     }
 }
